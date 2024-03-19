dc2db06483976e176491ab1b24898259aef3c5e6
El Yonko bravo <CPespinar@gmail.com>
2024-03-19 18:01:32 +0100

    Added styles to scrollbar in MovieCard

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,14 +16,33 @@ export default {
       gridTemplateColumns: {
         'auto-fit': 'repeat(auto-fit, minmax(150px, 1fr))',
       },
-      // add other theme extensions here
       textShadow: {
         'default': '2px 2px 4px rgba(0, 0, 0, 0.5)',
       },
+      scrollbar: (theme) => ({
+        DEFAULT: {
+          '&-thumb': {
+            backgroundColor: theme('colors.gray.400'), // Adjust thumb color
+            borderRadius: '9999px', // Fully rounded corners for the thumb
+            borderWidth: '2px',
+            borderColor: 'transparent', // Adjust thumb border color if needed
+            "&:hover": {
+              backgroundColor: theme('colors.gray.500'), // Adjust thumb color on hover
+            },
+          },
+          '&-track': {
+            backgroundColor: theme('colors.gray.100'), // Adjust track color
+            borderRadius: '9999px', // Fully rounded corners for the track
+          },
+        },
+      }),
     },
   },
-  variants: {},
+  variants: {
+    scrollbar: ['rounded'],
+  },
   plugins: [
-    require('tailwindcss-textshadow')
+    require('tailwindcss-textshadow'),
+    require('tailwind-scrollbar'), // Ensure this plugin is installed via npm or yarn
   ],
 }
