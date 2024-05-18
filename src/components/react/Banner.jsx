e0cdf2a1661bc6d82d2af2fe68dc689c7676508e
El Yonko bravo <CPespinar@gmail.com>
2024-05-18 15:26:59 +0200

    Text in Banner according to the movie

--- src/components/react/Banner.jsx
+++ src/components/react/Banner.jsx
@@ -1,48 +1,36 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-// Replace the first image import with the URL
-const comingSoonImage1 = new URL('../../Images/Oppenheimer.jpg', import.meta.url);
-const comingSoonImage2 = new URL('../../Images/Banner2.jpg', import.meta.url);
-const comingSoonImage3 = new URL('../../Images/Banner3.webp', import.meta.url);
-const comingSoonImage4 = new URL('../../Images/Banner4.webp', import.meta.url);
-const comingSoonImage5 = new URL('../../Images/Banner5.jpg', import.meta.url);
 
-
-export default function Banner() {
-     // Array of banner images
-     const bannerImages = [comingSoonImage1, comingSoonImage2, comingSoonImage3, comingSoonImage4, comingSoonImage5];
-
+export default function Banner({ films }) {
     // State to keep track of the active dot
     const [activeDot, setActiveDot] = useState(0);
-    const [activeImage, setActiveImage] = useState(bannerImages[0]);
 
     // Function to change the active dot and image
     const selectDot = (dotIndex) => {
         setActiveDot(dotIndex);
-        setActiveImage(bannerImages[dotIndex])
     };
 
     // Function to move to the next image
     const nextImage = () => {
         let nextIndex = activeDot + 1;
-        if (nextIndex >= bannerImages.length) {
+        if (nextIndex >= films.length) {
             nextIndex = 0; // Loop back to the first image
         }
         selectDot(nextIndex);
     };
 
     // Array to create dots dynamically, starting at index 0 to match the banner images
-    const dots = [0, 1, 2, 3, 4];
+    const dots = Array.from({ length: films.length }, (_, i) => i);
 
     return (
         <>
         {/* Enhanced Hero Image */}
         <div className="mb-6 mx-0 relative w-full rounded" style={{ height: '75vh' }}>
             {/* Use CSS transition for fade effect */}
-            {bannerImages.map((image, index) => (
+            {films.map((film, index) => (
                 <div key={index} className="absolute w-full h-full transition-opacity duration-1000 ease-in-out" style={{ opacity: activeDot === index ? 1 : 0 }}>
-                    <img src={image} alt="Banner" className="w-full h-full object-cover" />
+                    <img src={film.extended_info.poster_url} alt={film.locationInfo.title} className="w-full h-full object-cover" />
                 </div>
             ))}
 
@@ -51,32 +39,22 @@ export default function Banner() {
 
             {/* Text positioned like in the example image, constrained to not exceed 25% of the parent */}
             <div className="absolute inset-0 flex items-center pl-10 lg:pl-24 pr-20">
-                
                 <div className="w-1/4 space-y-4">
-
                     {/* Main Title */}
-                    <h1 className="text-6xl font-bold mb-4 text-white drop-shadow-xl">Loki</h1>
+                    <h1 className="text-6xl font-bold mb-4 text-white drop-shadow-xl">{films[activeDot].locationInfo.title}</h1>
 
                     {/* Subtitle */}
-                    <h2 className="text-3xl font-light text-gray-300">Titulo Original: Loki</h2>
+                    <h2 className="text-3xl font-light text-gray-300">Original Title: {films[activeDot].basic_info.original_title}</h2>
                     
                     {/* Sub | Dub Text */}
                     <div className="text-xl font-medium mb-2 text-red-400">Sub | Dub</div>
-                    {/* Description */}
-                    <p className="text-lg leading-relaxed text-gray-300">SERIE<br />
-                    Loki, el impredecible villano Loki (Hiddleston) regresa como el Dios del engaño en una nueva serie tras los acontecimientos de Avengers.
-                    </p>
 
-                    {/* Details */}
-                    <div className="text-sm text-gray-400">
-                        <p>Director: <span className="font-medium text-white">James Gunn</span></p>
-                        <p>Género: <span className="font-medium text-white">Acción, Aventura, C. Ficción, Disney</span></p>
-                        <p>Actores: <span className="font-medium text-white">Tom Hiddleston, Owen Wilson, Gugu Mbatha-Raw, Wunmi Mosaku, Eugene Cordero</span></p>
-                    </div>
+                    {/* Description */}
+                    <p className="text-lg leading-relaxed text-gray-300 line-clamp-4">{films[activeDot].locationInfo.synopsis}</p>
 
                     {/* Button */}
                     <button className="bg-red-500 hover:bg-red-700 text-white font-semibold px-6 py-3 rounded shadow transition ease-in-out duration-150">
-                        ENJOY MOVIES 
+                        ENJOY THIS MOVIE ONLINE
                     </button>
 
                     {/* Dots */}
@@ -102,8 +80,8 @@ export default function Banner() {
                     </div>
                 </div>
             </div>
+
             {/* Gradient Overlay */}
-            {/* I think the pointerEvents can be improve by placing this div in a correct place */}
             <div className="absolute inset-0 z-0" style={{ pointerEvents: 'none' }}> 
                 <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-b from-transparent via-transparent to-gray-900"></div>
             </div>
