b2f6e3aab09bbf69c64350f501f07836e8c18ced
Eric Cañas <elcorreodeharu@gmail.com>
2024-06-10 00:25:39 +0200

    Translations for the movie staff roles

--- src/utils/localization.ts
+++ src/utils/localization.ts
@@ -5,23 +5,29 @@ const translations = (await getEntry('location', 'translations')).data
 const __availableLanguages: Record<string, boolean> = (await getEntry('location', 'available-languages')).data;
 export const AVAILABLE_LANGUAGES: string[] = Object.keys(__availableLanguages).filter(lang => __availableLanguages[lang]);
 
-export function getTranslationFunction(lang: string) {
-    return (word: string) => t(word, lang)
+export function getTranslationFunction(lang: string, warn: boolean = true) {
+    return (word: string) => t(word, lang, warn)
 }
 
-function t(word: string, lang: string) {
+function t(word: string, lang: string, warn: boolean = true) {
     if (!translations[word] && translations[word.toLowerCase()]) {
-        console.warn(`Translation for "${word}" not found, found the lowercase version "${word.toLowerCase()}" instead.`);
+        if (warn){
+            console.warn(`Translation for "${word}" not found, found the lowercase version "${word.toLowerCase()}" instead.`);
+        }
         word = word.toLowerCase();
     }
     
     let translatedWord: string | null  = null;
 
     if (!translations[word]) {
-        console.warn(`Translation for "${word}" not found [in any language]`);
+        if (warn){
+            console.warn(`Translation for "${word}" not found [in any language]`);
+        }
     }
     else if (!translations[word][lang]) {
-        console.warn(`Translation for "${word}" not found in "${lang}"`);
+        if (warn){
+            console.warn(`Translation for "${word}" not found in "${lang}"`);
+        }
     } else {
         translatedWord = translations[word][lang];
     }
