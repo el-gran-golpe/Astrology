7a2a5db6d0c00b330ac0210cc24f095740ad1006
Eric Cañas <elcorreodeharu@gmail.com>
2024-06-29 10:28:04 +0200

    Load also from env in searchbar

--- build_scripts/searchbar_suggestions.js
+++ build_scripts/searchbar_suggestions.js
@@ -5,8 +5,33 @@ import path from 'path';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, query, orderBy } from 'firebase/firestore';
 
-const firebaseConfigPath = './src/content/secrets/firebase.json';
-const firebaseConfig = JSON.parse(fs.readFileSync(firebaseConfigPath, 'utf8'));
+import { config } from "dotenv";
+//import { getAnalytics } from "firebase/analytics";
+
+// Load environment variables from .env file
+config();
+
+
+// Retrieve Firebase configuration from environment variables
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+// Check if all required environment variables are present
+if (!firebaseConfig.apiKey ||
+    !firebaseConfig.authDomain ||
+    !firebaseConfig.projectId ||
+    !firebaseConfig.storageBucket ||
+    !firebaseConfig.messagingSenderId ||
+    !firebaseConfig.appId ||
+    !firebaseConfig.measurementId) {
+    throw new Error("Missing Firebase configuration in environment variables.");
+}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
